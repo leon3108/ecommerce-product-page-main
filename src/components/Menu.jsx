c3f626422c7ff3d650a931a0f99aa3bb011ec26c
Maxime NOEL <maxime.noel@epitech.eu>
2022-12-02 14:05:06 +0100

    [ADD] hide the product when menu is displayed

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,19 +3,24 @@ import { useContext } from 'react';
 import UserContext from './UserContext';
 
 function Menu() {
-  const {showMenu, setShowMenu} = useContext(UserContext);
-    return (
-        <div style={{width: 250 + 'px', height:100 + '%', backgroundColor: 'white', position:'absolute', zIndex:'1000'}}>
-            <img src={cross} alt="menu déroulant" id="iconMenu" style={{marginLeft:20 + 'px', marginTop: 20 + 'px'}} onClick={() => (setShowMenu(false))}/>
-            <div style={{display:'flex', flexDirection:'column', justifyContent:'space-between'}}>
-                <p>Collections</p>
-                <p>Men</p>
-                <p>Women</p>
-                <p>About</p>
-                <p>Contact</p>
-            </div>
-        </div>
-    )
+	const { showMenu, setShowMenu } = useContext(UserContext);
+	return (
+		<>
+			<div id='tmp' style={{backgroundColor: 'black', height:100+'%', position:'absolute', zIndex:'1000', width:100+'%', opacity:'0.75'}}>okkkkk</div>
+			<div className='menu' style={{ width: 250 + 'px', height: 100 + '%', backgroundColor: 'white', position: 'absolute', zIndex: '1000' }}>
+				<img src={cross} alt="menu déroulant" id="iconMenu" style={{ marginLeft: 20 + 'px', marginTop: 20 + 'px' }} onClick={() => (setShowMenu(false))} />
+				<div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+					<p>Collections</p>
+					<p>Men</p>
+					<p>Women</p>
+					<p>About</p>
+					<p>Contact</p>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default Menu
+
+
