6b6617cd03ba11a9de2c55a583d9c94ca388e2e7
Maxime NOEL <maxime.noel@epitech.eu>
2022-12-02 11:10:40 +0100

    [ADD] can view all products pictures

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,19 +1,36 @@
-import product from '../images/image-product-1.jpg';
-// import previous from "../images/icon-previous.svg";
+import product1 from '../images/image-product-1.jpg';
+import product2 from '../images/image-product-2.jpg';
+import product3 from '../images/image-product-3.jpg';
+import product4 from '../images/image-product-4.jpg';
+import previous from "../images/icon-previous.svg";
 import IconPrevious from "./Icon_previous";
 import '../styles/Product.css'
 
 
 import next from "../images/icon-next.svg";
+import { useState } from 'react';
 
 function Product() {
+    const products = [
+        product1,
+        product2,
+        product3,
+        product4
+    ]
+    const [productIndex, setProdcuctIndex] = useState(0);
+
+    function changeimage(index) {
+        if (productIndex + index >= 0 && productIndex + index < products.length)
+            setProdcuctIndex(productIndex + index)
+    }
+
     return (
     <div className='product'>
         <div className='divProduct' >
-            <img id="imageproduct" src={product} alt="produit"/>
-            {/* <img id="icon-previous" src={previous} alt="previous icon"/> */}
-            <IconPrevious/>
-            <img id="icon-next" src={next} alt="next icon" />
+            <img id="imageproduct" src={products[productIndex]} alt="produit"/>
+            <img id="icon-previous" src={previous} alt="previous icon" onClick={() => (changeimage(-1))}/>
+            {/* <IconPrevious/> */}
+            <img id="icon-next" src={next} alt="next icon" onClick={() => (changeimage(+1))}/>
         </div>
     </div>
     )
